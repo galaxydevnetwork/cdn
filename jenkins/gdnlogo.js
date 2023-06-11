2566966cacab4c731b703ced1857427d5e462858
galaxydevel0per <58356708+Galaxy202@users.noreply.github.com>
2023-06-11 10:31:24 +0400

    Create gdnlogo.js

---
+++ jenkins/gdnlogo.js
@@ -0,0 +1,2 @@
+document.getElementById("jenkins-head-icon").src = 'https://github.com/galaxydevnetwork/cdn/blob/main/img/GalaxyDevNetwork%20Logo.png?raw=true';
+document.getElementById("jenkins-name-icon").remove();
